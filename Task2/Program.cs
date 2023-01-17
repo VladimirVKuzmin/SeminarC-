0186d4fa87eb5d669fe58fa32ace9956277f6565
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-17 13:15:37 +0700

    name day of week by number

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+System.Console.WriteLine();
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+System.Console.Write("Введите номер дня недели:   ");
+string writeDay = Console.ReadLine();
+bool result = int.TryParse(writeDay, out int dayNumber);
+
+System.Console.WriteLine();
+if (result == false) System.Console.WriteLine("Число введено неверно");
+else if (0 < dayNumber && dayNumber < 8) System.Console.WriteLine($"{dayNumber}-й день недели - {Days[dayNumber - 1]}");
+else System.Console.WriteLine("Дня недели с таким номером не существует");
+System.Console.WriteLine();
