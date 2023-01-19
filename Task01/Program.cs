0ecc9fd4ca9f3226193dfd6c41b1c18e9afa9529
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-19 17:52:26 +0700

    Task 01 random number

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 25=>5
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+int firstNum = num / 10;
+int secondNum = num % 10;
+if (firstNum < secondNum)
+{
+    Console.WriteLine(secondNum);
+}
+else
+{
+    Console.WriteLine(firstNum);
+}
+
+
