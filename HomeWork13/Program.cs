accfe5f3923e0bf499acee6524eb41b66ae2e765
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-20 21:54:06 +0700

    find third number

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Вывести третью цифру заданного числа или сообщить, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (ThirdDigit(number1) == -1)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра {ThirdDigit(number1)}");
+}
+int ThirdDigit(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+            result = number % 10;
+        }
+        return result; 
+    }
