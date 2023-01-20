f94b4cea8fe44b5ea53b3a30fad1d592560f5800
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-20 16:32:25 +0700

    number day of week, weekend

---
+++ HomeWork15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.Write("Введите число от 1 до 7:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >0 && num < 8)
+{
+    if (num == 6 || num == 7)
+    {
+        Console.WriteLine("Сегодня выходной");
+    }
+    else 
+    {
+        Console.WriteLine("Сегодня рабочий день");
+    }
+}
