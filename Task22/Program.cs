7c1155fc13cd91c84fba461c8792437924664b38
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-24 13:42:47 +0700

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+int num = new Random().Next(1, 21);
+System.Console.WriteLine($"num = {num}");
+for( int i = 1; i <= num; i++)
+{
+    Console.WriteLine(Math.Pow(i, 2));
+}
