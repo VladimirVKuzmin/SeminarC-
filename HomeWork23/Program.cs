62b8594b6e77aa47997f2ed940e5f94921d4d6ca
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-24 14:05:23 +0700

    SquareTable

---
+++ HomeWork23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+int num = new Random().Next(1, 10);
+System.Console.WriteLine($"num = {num}");
+for( int i = 1; i <= num; i++)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+}
