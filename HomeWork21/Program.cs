2e93588a30fbecded0fcb4a8729376ab74ccc976
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-24 13:53:13 +0700

    Distance in 3D place

---
+++ HomeWork21/Program.cs
@@ -0,0 +1,17 @@
+﻿// // Напишите программу, которая принимает 
+// // на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// // A (3,6,8); B (2,1,-7), -> 15.84
+// // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Random rnd = new Random();
+int x1 = rnd.Next(-10, 11);
+int y1 = rnd.Next(-10, 11);
+int x2 = rnd.Next(-10, 11);
+int y2 = rnd.Next(-10, 11);
+int z1 = rnd.Next(-10, 11);
+int z2 = rnd.Next(-10, 11);
+
+Console.WriteLine($"x1 {x1}, y1 {y1}, x2 {x2}, y2 {y2} z1 {z1}, z2 {z2}");
+double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+Console.WriteLine(result);
+
