e94869f0b7a00b0c372ceb55202648d7baf6a89b
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-24 13:03:05 +0700

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,16 @@
+﻿// // Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+Random rnd = new Random();
+int x1 = rnd.Next(-10, 11);
+int y1 = rnd.Next(-10, 11);
+int x2 = rnd.Next(-10, 11);
+int y2 = rnd.Next(-10, 11);
+
+Console.WriteLine($"x1 {x1}, y1 {y1}, x2 {x2}, y2 {y2}");
+double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine(result);
+
+
