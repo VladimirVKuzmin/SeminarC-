c2e3455cf49d7700277ff31622d007e63600ed8e
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-25 16:08:52 +0700

    Check Pallindrom

---
+++ HomeWork19/Program.cs
@@ -0,0 +1,23 @@
+﻿// // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int number, remind, sum = 0, temp;
+Console.WriteLine("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+temp = number;
+while (number > 0)
+{
+    remind = number % 10;
+    number = number / 10;
+    sum = sum * 10 + remind;
+}
+    if (temp == sum)
+   {
+      Console.WriteLine("Число являеется палиндромом");
+   }
+   else
+   {
+      Console.WriteLine("Число не являеется палиндромом");
+   }
