0838cb1a0a8a81847c3797a8ccc8b5105ce0cfcb
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-25 16:21:44 +0700

    Find Third Number add 2nd var

--- HomeWork13/Program.cs
+++ HomeWork13/Program.cs
@@ -1,26 +1,37 @@
 ﻿// Вывести третью цифру заданного числа или сообщить, что третьей цифры нет.
 
-Console.WriteLine("Введите число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
 
-if (ThirdDigit(number1) == -1)
+// if (ThirdDigit(number1) == -1)
+// {
+//     Console.WriteLine("третьей цифры нет");
+// }
+// else
+// {
+//     Console.WriteLine($"Третья цифра {ThirdDigit(number1)}");
+// }
+// int ThirdDigit(int number)
+// {
+//     int result = -1;
+//     if (number >= 100)
+//     {
+//         while (number > 999)
+//         {
+//             number = number / 10;
+//         }
+//             result = number % 10;
+//     }
+//     return result; 
+// }
+
+
+// второй вариант
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while(num >= 1000)
 {
-    Console.WriteLine("третьей цифры нет");
+    num /=10;
 }
-else
-{
-    Console.WriteLine($"Третья цифра {ThirdDigit(number1)}");
-}
-int ThirdDigit(int number)
-{
-    int result = -1;
-    if (number >= 100)
-    {
-        while (number > 999)
-        {
-            number = number / 10;
-        }
-            result = number % 10;
-    }
-    return result; 
-}
+System.Console.WriteLine(num %10);
