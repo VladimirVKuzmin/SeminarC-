f9b6a83fefc6cf2929b2fda2a47d8ae5476d3bad
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-27 19:08:15 +0700

    Sum og digits in number

---
+++ HomeWork27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Sum(X);
+void Sum(int number)
+{
+    int sum = 0;
+    for (int i = 0; i < number; i++)
+    {
+    sum = sum + number % 10;
+    number = number / 10;
+    }
+    
+    Console.WriteLine($"Сумма цифр в числе {X} равна {sum}");
+}
