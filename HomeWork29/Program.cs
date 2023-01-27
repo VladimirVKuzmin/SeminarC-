0f8469b8648983d362cf5cc0490c93f024f2ed93
VladimirK <vladimir1kuzmin@gmail.com>
2023-01-27 16:50:07 +0700

    Array 8 Elements

---
+++ HomeWork29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int N = 8;
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+    Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(SetArray(N));
