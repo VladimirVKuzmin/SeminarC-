072c039ca6177c553168507e3c768f8348a14643
VladimirK <vladimir1kuzmin@gmail.com>
2023-02-01 20:06:22 +0700

    DifferenceMinMax

---
+++ HomeWork38/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] GetRandomArray(int size, int min, int max)
+{
+    int[] RandomArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        RandomArray[i] = new Random().Next(min, max);
+    }
+    return RandomArray;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+int MaxNum(int[] array)
+{
+    int max = array[0];
+    for(int i=0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int MinNum(int[] array)
+{
+    int min = array[0];
+    for(int i=0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = GetRandomArray (size,0,100);
+
+int max = MaxNum(array);
+int min = MinNum(array);
+
+PrintArray(array);
+Console.WriteLine(max-min);
