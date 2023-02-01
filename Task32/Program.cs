eaf27451d13135e2367505aabaeb526673c621ed
VladimirK <vladimir1kuzmin@gmail.com>
2023-02-01 20:29:51 +0700

    Replace -/+ elements of the array

---
+++ Task32/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные 
+// элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+PrintArray(ReplaceNum(arr));
+
+int[] ReplaceNum(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+int GetSumLessZero(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        {
+            result += arr[i];
+        }
+    }
+    return result;
+}
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++) 
+    {
+        result[i] = new Random().Next(-20, 21);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
