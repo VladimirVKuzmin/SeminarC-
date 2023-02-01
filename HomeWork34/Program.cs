93417fbfbb9eeebddd93833aa2ac2e7242fdbb77
VladimirK <vladimir1kuzmin@gmail.com>
2023-02-01 20:12:26 +0700

    Count Even Numbers

---
+++ HomeWork34/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] arr = GetRandomArray(20);
+PrintArray(arr);
+Console.WriteLine(FillArray(arr));
+
+int FillArray(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+            Console.Write($"[{array[i]}]");
+        }
+    }
+    return count;
+}
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++) 
+    {
+        result[i] = new Random().Next(1, 1000);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
