abc7117f82dbb658cf638e5357aed4c9810c8dd2
VladimirK <vladimir1kuzmin@gmail.com>
2023-02-11 03:01:44 +0700

    FindNumber

---
+++ HomeWork52/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите колличество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите колличество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] arr = GetArray(rows, columns, 1, 10);
+PrintArray(arr);
+Console.WriteLine();
+FindArraySumColumns(arr);
+
+void FindArraySumColumns(int[,] arr)
+{
+    double[] array = new double[arr.GetLength(1)];
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0.0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i, j];
+        }
+        array[j] = Math.Round(sum / arr.GetLength(0), 1);
+    }
+    Console.WriteLine($"Среднее арифметическое каждого столбца: {string.Join("; ",array)}");
+    Console.WriteLine();
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for(int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 20);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] arr)
+{
+        for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
