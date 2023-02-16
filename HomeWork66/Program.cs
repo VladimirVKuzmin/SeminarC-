7f2bd462072ed15e3956dc5723bd70f7b714dac6
VladimirK <vladimir1kuzmin@gmail.com>
2023-02-16 18:24:04 +0700

    Find sum of elements from M to N

---
+++ HomeWork66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+int sum = 0;
+
+Console.WriteLine($"Сумма элементов равна {SumOfElements(M, N, sum)}");
+
+int SumOfElements(int m, int n, int count)
+{
+    if (m == n) return m;
+    return count += (m++) + SumOfElements(m, n, count);
+}
